e882e869c43a775ee2825674ad5e913243a52512
Marcus Posey <marcusposey@gmail.com>
2016-01-29 22:36:42 -0600

    Created IFileParser interface

---
+++ Genetics/FileParser.h
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+
+/**
+ * The IFileParser struct provides an interface for reading
+ * genotype information from various file formats.
+ */
+struct IFileParser {
+	virtual void parseFile(std::vector<std::string>& genotypes,
+		int expectedCount) = 0;
+};
