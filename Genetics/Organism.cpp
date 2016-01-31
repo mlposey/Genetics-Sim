590a340e6d5d4f7059138a6033b57e6835a38478
Marcus Posey <marcusposey@gmail.com>
2016-01-30 18:16:46 -0600

    Added blank methods and fields to Organism

--- Genetics/Organism.cpp
+++ Genetics/Organism.cpp
@@ -1,11 +1,14 @@
 #include "Organism.h"
 
-
-Organism::Organism(void)
-{
+Organism::Organism(const string& genus, const string& species, const string& name) {
 }
 
+void Organism::addGene(const Gene& gene) {
+}
 
-Organism::~Organism(void)
-{
+void Organism::serveGene(Gene& g) const {
+}
+
+string Organism::toString() const {
+	return "";
 }
--- Genetics/Organism.h
+++ Genetics/Organism.h
@@ -1,8 +1,26 @@
 #pragma once
+#include <vector>
+
+#include "Gene.h"
+
+using std::string;
+
 class Organism
 {
 public:
-	Organism(void);
-	~Organism(void);
+	Organism(const string &genus, const string &species, const string &name);
+
+	void addGene(const Gene &gene);
+
+	void serveGene(Gene &g) const;
+
+	string toString() const;
+
+private:
+	string _genus;
+	string _species;
+	string _name;
+
+	std::vector<Gene> _genotype;
 };
 
