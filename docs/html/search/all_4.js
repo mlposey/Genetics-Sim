47275f1fd0d3a8b00f03630698b6eb60911fb5b4
Marcus Posey <marcusposey@gmail.com>
2016-02-05 13:19:01 -0600

    Rebuilt doxygen docs to reflect IFileParser deletion

--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['ifileparser',['IFileParser',['../struct_i_file_parser.html',1,'']]],
   ['iobserver',['IObserver',['../struct_i_observer.html',1,'']]],
   ['iobserver_3c_20organism_20_3e',['IObserver&lt; Organism &gt;',['../struct_i_observer.html',1,'']]],
   ['isdominant',['isDominant',['../class_allele.html#a6096e43a00dfc291544abd97b4a82c06',1,'Allele']]]
