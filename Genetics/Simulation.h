b46dc6b6f66de3fe4e7d26c69e0d34ebac9c5ff2
Marcus Posey <marcusposey@gmail.com>
2016-02-05 13:50:49 -0600

    Created and generated doxygen docs for Simulation

--- Genetics/Simulation.h
+++ Genetics/Simulation.h
@@ -5,11 +5,21 @@
 #include "LoveChamber.h"
 #include "StatCounter.h"
 
+/**
+ * @brief The Simulation class handles the main thread of execution for the genetic simulation
+
+ * Simulation invokes the necessary file parser to read the initial parent
+ * states and uses this data to start the LoveChamber and StatCounter which
+ * handle all reproduction and data collection
+ * @see LoveChamber, StatCounter
+ */
 class Simulation
 {
 public:
+	/// Constructs a Simulation object and initializes the state of the program
 	Simulation();
 
+	/// Runs through the entirety of the simulation and then prints the results
 	void run();
 
 private:
--- docs/html/search/all_a.js
+++ docs/html/search/all_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['removeobserver',['removeObserver',['../class_observable.html#a88784c910caefa3a830025209d71adb3',1,'Observable']]]
+  ['removeobserver',['removeObserver',['../class_observable.html#a88784c910caefa3a830025209d71adb3',1,'Observable']]],
+  ['run',['run',['../class_simulation.html#ae5c367f87c0b5dc9740bc6d00e44e72c',1,'Simulation']]]
 ];
