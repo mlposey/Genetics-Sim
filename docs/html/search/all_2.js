73794d4252b64b02d8a38e516b23797b221f2f4f
Marcus Posey <marcusposey@gmail.com>
2016-02-04 21:55:47 -0600

    Rebuilt docs to exclude deleted files

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['first',['first',['../class_gene.html#a935e5f290b1e66b970ed8b52a97b1ae9',1,'Gene::first() const '],['../class_gene.html#a935e5f290b1e66b970ed8b52a97b1ae9',1,'Gene::first() const ']]]
+  ['first',['first',['../class_gene.html#a935e5f290b1e66b970ed8b52a97b1ae9',1,'Gene']]]
 ];
--- docs/html/search/functions_8.js
+++ docs/html/search/functions_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['second',['second',['../class_gene.html#a7c744b5c6e8d305c47c1563cc0e2acc8',1,'Gene::second() const '],['../class_gene.html#a7c744b5c6e8d305c47c1563cc0e2acc8',1,'Gene::second() const ']]],
+  ['second',['second',['../class_gene.html#a7c744b5c6e8d305c47c1563cc0e2acc8',1,'Gene']]],
   ['servegene',['serveGene',['../class_organism.html#aa6d906d352590a2f26f40bd953245e98',1,'Organism']]]
 ];
