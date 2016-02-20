de1c6d904ba57a55b93f3faddb0b52f15044907b
Marcus Posey <marcusposey@gmail.com>
2016-02-20 17:05:00 -0600

    Created unit test for Gene::getZygosity

--- tests/gene_tests.cpp
+++ tests/gene_tests.cpp
@@ -18,8 +18,32 @@ public:
  * This test assumes the following behavior:
  * 1.) The phenotype of a gene is the description of the dominant Allele
  *
- * Deviation from the above should result in a failed test
+ * Deviation from the above should result in a failed test.
  */
 TEST_F(GeneTests, getPhenotype) {
 	ASSERT_EQ("dominant description", gene.getPhenotype());
+}
+
+
+/**
+ * This test assumes the following behavior:
+ * The zygosity of a gene is:
+ * 1.) "heterozygous dominant" if the Alleles do not match
+ * 2.) "homozygous recessive" if both Alleles are recessive
+ * 3.) "homozygous dominant" if both Alleles are dominant.
+ *
+ * Deviation from the above should result in a failed test.
+ */
+TEST_F(GeneTests, getZygosity) {
+	Allele a1Copy(a1);
+	Allele a2Copy(a2);
+
+	Gene homozygousRecessive(a2, a2Copy, "desc");
+	ASSERT_EQ("homozygous recessive", homozygousRecessive.getZygosity());
+
+	Gene homozygousDominant(a1, a1Copy, "desc");
+	ASSERT_EQ("homozygous dominant", homozygousDominant.getZygosity());
+
+	// gene is declared in the class fixture as "Tt"
+	ASSERT_EQ("heterozygous dominant", gene.getZygosity());
 }
