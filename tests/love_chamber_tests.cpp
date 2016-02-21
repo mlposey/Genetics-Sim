1fb3c479ffbe2e0bf25d54c05c722819236a7263
Marcus Posey <marcusposey@gmail.com>
2016-02-21 01:16:53 -0600

    Created function to compare stdout output

--- tests/love_chamber_tests.cpp
+++ tests/love_chamber_tests.cpp
@@ -25,45 +25,23 @@ public:
  * Deviation from the above should result in a failing test.
  */
 TEST_F(LoveChamberTests, constructor) {
-    // Output console data here
-    const char *consoleOutputFileName = "console_output.txt";
-
-    // LoveChamber::LoveChamber() should ouput the organism data to stdout
-    // We are interested in if it was printed correctly, so we redirect stdout
-    // to this file.
-    FILE *file = fopen(consoleOutputFileName, "w");
-	swapOutput(stdout, file);
-
-    LoveChamber chamber(o1, o2);
-
-    // Redirect output back to stdout
-	swapOutput(stdout, file);
-	fclose(file);
-
     // Even in the event that the organisms themselves cannot properly print
     // data, we still expect the LoveChamber to output this basic information.
-    const char* expectedInformation[] = {
-        "\n-----------------------Parent Data-----------------\n",
-        "Sim parent 1\n",
-        // parent1.printDescription() should put information here
-        "Sim parent 2\n",
-        // parent2.printDescription() should put information here
-        "---------------------------------------------------\n\n"
-    };
+    std::vector<const char*> expected;
+    expected.push_back("\n-----------------------Parent Data-----------------\n");
+    expected.push_back("Sim parent 1\n");
+    expected.push_back("Sim parent 2\n");
+    expected.push_back("---------------------------------------------------\n\n");
 
-    std::ifstream consoleOuputFile(consoleOutputFileName);
-    ASSERT_TRUE(consoleOuputFile.is_open());
+    ASSERT_TRUE(compareOutput([&](){ LoveChamber(o1, o2); }, expected));
+}
 
-    std::stringstream ss;
-    ss << consoleOuputFile.rdbuf();
-    std::string fileContents = ss.str();
+/**
+ * This test expects the following behavior:
+ * 1.)
+ *
+ * Deviation from the above should result in a failed test.
+ */
+TEST_F(LoveChamberTests, mate) {
 
-    // Check that each component of the expected output was printed
-    for (auto str : expectedInformation) {
-        ASSERT_TRUE(fileContents.find(str) != -1);
-    }
-
-    // Close and delete the file
-    consoleOuputFile.close();
-    std::remove(consoleOutputFileName);
 }
--- tests/utils.h
+++ tests/utils.h
@@ -1,15 +1,39 @@
 #include <cstdio>
 #include <cstring>
+#include <functional>
 
-/**
- * This function can be used to redirect stdout to files
- *
- * The first call to swapOutput redirects a to b
- * The second call redirects b to a
- */
-inline void swapOutput(FILE *a, FILE *b) {
-	FILE tmp;
-	memcpy(&tmp, a, sizeof(FILE));
-	memcpy(a, b, sizeof(FILE));
-	memcpy(b, &tmp, sizeof(FILE));
+bool compareOutput(const std::function<void(void)> &callback,
+                   const std::vector<const char*> &expectedOutputs) {
+    static void (*swap_)(FILE*, FILE*) = [](FILE *a, FILE *b) {
+        FILE tmp;
+        memcpy(&tmp, a, sizeof(FILE));
+        memcpy(a, b, sizeof(FILE));
+        memcpy(b, &tmp, sizeof(FILE));
+    };
+
+	const char *fn = "console_output.txt";
+
+    FILE *tmp = fopen(fn, "w");
+    swap_(stdout, tmp);
+    callback();
+    swap_(stdout, tmp);
+    fclose(tmp);
+
+    std::ifstream consoleOutput(fn);
+    if (!consoleOutput.good())
+        return false;
+
+    std::stringstream ss;
+    ss << consoleOutput.rdbuf();
+
+    std::string contents = ss.str();
+
+	consoleOutput.close();
+	std::remove(fn);
+
+    for (auto str : expectedOutputs) {
+        if (contents.find(str) == -1)
+            return false;
+    }
+    return true;
 }
