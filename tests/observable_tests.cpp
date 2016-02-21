06261088b57e06f18f10437af574f105384aaaac
Marcus Posey <marcusposey@gmail.com>
2016-02-21 13:44:59 -0600

    Created test for Observable::removeObserver

--- tests/observable_tests.cpp
+++ tests/observable_tests.cpp
@@ -39,3 +39,33 @@ TEST_F(ObservableTests, addObserver) {
 
     ASSERT_EQ(2, observable.getObserverCount());
 }
+
+/**
+ * This test expects the following behavior:
+ * 1.) Observable::removeObserver removes the supplied argument and all copies
+ * 2.) Observable::removeObserver DOES NOT remove observers that were not
+ *     specified
+ *
+ * Deviation from the above should result in a failed test.
+ */
+TEST_F(ObservableTests, removeObserver) {
+    ASSERT_EQ(0, observable.getObserverCount());
+
+    observable.addObserver(observer);
+    observable.addObserver(observer);
+
+    MockObserver other;
+    observable.addObserver(other);
+
+    ASSERT_EQ(3, observable.getObserverCount());
+
+    observable.removeObserver(observer);
+
+    // 'other' should still remain
+    ASSERT_EQ(1, observable.getObserverCount());
+    ASSERT_TRUE(observable.contains(other));
+    
+    ASSERT_FALSE(observable.contains(observer));
+}
+
+// TODO: Create test for Observable::contains
