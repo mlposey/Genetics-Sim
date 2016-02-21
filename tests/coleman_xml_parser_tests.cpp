ff6555b5f1c29c7217e8c9e0bce6f7d857cc22fc
Marcus Posey <marcusposey@gmail.com>
2016-02-20 20:54:33 -0600

    Create unit test for ColemanXMLParser constructor

--- tests/coleman_xml_parser_tests.cpp
+++ tests/coleman_xml_parser_tests.cpp
@@ -1,5 +1,51 @@
 #include <gtest/gtest.h>
+#include <utility>
+#define name(FILENAME, EXISTS) std::make_pair(FILENAME, EXISTS)
 
+#include <vector>
+#include <fstream>
+
+#include "../Genetics/GeneticsSimDataParser.cpp"
 #include "../Genetics/ColemanXMLParser.cpp"
 
-class ColemanXMLParserTests : public testing::Test {};
+class ColemanXMLParserTests : public testing::Test {
+public:
+    // All the files to test
+    std::vector<std::pair<const char*, bool/*exists*/>> fileNames;
+
+    ColemanXMLParserTests() {
+        fileNames = {
+            name("GeneticsSim1.xml", true),
+            name("NonexistantFile.xml", false),
+            name("", false),
+            name(".xml", false),
+            name("32", false)
+        };
+    }
+};
+
+/**
+ * This test expects the following behavior:
+ * 1.) An exception is thrown if the filename provided is for a nonexistant file
+ *
+ * Deviation from the above should result in a failed test.
+ */
+TEST_F(ColemanXMLParserTests, constructor) {
+    for (auto &fileName : fileNames) {
+        bool isExceptionThrown = false;
+        try {
+            ColemanXMLParser parser(fileName.first);
+        } catch (std::ifstream::failure &e) {
+            isExceptionThrown = true;
+        }
+
+        if (!isExceptionThrown) {
+            // The file should have existed
+            ASSERT_TRUE(fileName.second);
+        }
+        else {
+            // The file should not have existed
+            ASSERT_TRUE(!fileName.second);
+        }
+    }
+}
