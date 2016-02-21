658400db15b8b4d1a7918af6945169cadbe9caa6
Marcus Posey <marcusposey@gmail.com>
2016-02-20 23:33:36 -0600

    Created unit test for constructor. It's not really working right now

--- tests/love_chamber_tests.cpp
+++ tests/love_chamber_tests.cpp
@@ -1,5 +1,66 @@
 #include <gtest/gtest.h>
+#include <fstream>
+#include <cstdio>
+#include <sstream>
+#include <vector>
 
 #include "../Genetics/LoveChamber.cpp"
 
-class LoveChamberTests : public testing::Test {};
+class LoveChamberTests : public testing::Test {
+public:
+    Organism o1, o2;
+
+    LoveChamberTests()
+        : o1("Pisum", "Sativum", "Pea Plant")
+        , o2(o1)
+    {}
+};
+
+/**
+ * This test expects the following behavior:
+ * 1.) The constructor takes the organisms and prints to the screen both of
+ *     their descriptions
+ *
+ * Deviation from the above should result in a failing test.
+ */
+TEST_F(LoveChamberTests, constructor) {
+    // Output console data here
+    const char *consoleOutputFileName = "console_output.txt";
+
+    // LoveChamber::LoveChamber() should ouput the organism data to stdout
+    // We are interested in if it was printed correctly, so we redirect stdout
+    // to this file.
+    freopen(consoleOutputFileName, "w", stdout);
+
+    LoveChamber chamber(o1, o2);
+
+    // Close the file so we can examine it
+    fclose(stdout);
+
+    // Even in the event that the organisms themselves cannot properly print
+    // data, we still expect the LoveChamber to output this basic information.
+    std::vector<const char*> expectedInformation = {
+        "\n-----------------------Parent Data-----------------\n",
+        "Sim parent 1\n",
+        // parent1.printDescription() should put information here
+        "Sim parent 2\n",
+        // parent2.printDescription() should put information here
+        "---------------------------------------------------\n\n"
+    };
+
+    std::ifstream consoleOuputFile(consoleOutputFileName);
+    ASSERT_TRUE(consoleOuputFile.is_open());
+
+    std::stringstream ss;
+    ss << consoleOuputFile.rdbuf();
+    std::string fileContents = ss.str();
+
+    // Check that each component of the expected output was printed
+    for (auto str : expectedInformation) {
+        ASSERT_TRUE(fileContents.find(str) != -1);
+    }
+
+    // Close and delete the file
+    consoleOuputFile.close();
+    std::remove(consoleOutputFileName);
+}
