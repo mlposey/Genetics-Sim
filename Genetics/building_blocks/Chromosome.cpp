06f2d51fde7805088dae0291369d86e53b020376
Marcus Posey <marcusposey@gmail.com>
2016-03-15 11:03:24 -0500

    Created iterator for chromosome

--- Genetics/building_blocks/Chromosome.cpp
+++ Genetics/building_blocks/Chromosome.cpp
@@ -1,13 +1,26 @@
 #include "Chromosome.h"
 
 void Chromosome::addGene(const Gene &gene) {
-  _genes.push_back(gene);
+    _genes.push_back(gene);
 }
 
 Chromosome::const_iterator Chromosome::begin() const {
-  return Chromosome::const_iterator();
+    return Chromosome::const_iterator(_genes.begin());
 }
 
 Chromosome::const_iterator Chromosome::end() const {
-  return Chromosome::const_iterator();
+    return Chromosome::const_iterator(_genes.end());
+}
+
+bool Chromosome::const_iterator::operator!=(const Chromosome::const_iterator &rhs) {
+    return _it != rhs._it;
+}
+
+char Chromosome::const_iterator::operator*() const {
+    return _it->getRandomAllele().getSymbol();
+}
+
+Chromosome::const_iterator &Chromosome::const_iterator::operator++(int c) {
+    ++_it;
+    return *this;
 }
--- Genetics/building_blocks/Chromosome.h
+++ Genetics/building_blocks/Chromosome.h
@@ -7,7 +7,17 @@ class Chromosome {
 public:
     class const_iterator {
     public:
+        const_iterator(std::vector<Gene>::const_iterator it)
+                : _it(it) { }
+
+        bool operator!=(const const_iterator &rhs);
+
+        char operator*() const;
+
+        const_iterator &operator++(int c);
+
     private:
+        std::vector<Gene>::const_iterator _it;
     };
 
     /**
@@ -17,6 +27,7 @@ public:
     void addGene(const Gene &gene);
 
     const_iterator begin() const;
+
     const_iterator end() const;
 
 private:
