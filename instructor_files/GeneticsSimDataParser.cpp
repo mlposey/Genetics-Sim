fbec94cb628fb25e5f03fbcdbb38ad8a7fa0f3c6
Marcus Posey <marcusposey@gmail.com>
2016-03-16 17:48:10 -0500

    Replaced old v1.0 instructor files with new versions

--- instructor_files/GeneticsSimDataParser.cpp
+++ instructor_files/GeneticsSimDataParser.cpp
@@ -4,7 +4,7 @@
 // Author: Dr. Rick Coleman
 // Date: December 2009
 //====================================================================
-#pragma warning(disable:4996)
+#define _CRT_SECURE_NO_WARNINGS
 
 #include "GeneticsSimDataParser.h"
 #include <string.h>
@@ -12,13 +12,42 @@
 //-----------------------------------
 // Constructor
 //-----------------------------------
-GeneticsSimDataParser::GeneticsSimDataParser(char *fileName)
+GeneticsSimDataParser::GeneticsSimDataParser()
 {
 	// Initialize everything
-	char line[128];
+	m_iNumChromosomes = 0;
 	m_iNumGenes = 0;
 	m_iNextGeneNumber = 0;
-	m_iNextParentNumber = 0;
+	m_iNextChNumP1 = 0;
+	m_iNextChNumP2 = 0;
+}
+
+//-----------------------------------
+// Destructor
+//-----------------------------------
+GeneticsSimDataParser::~GeneticsSimDataParser()
+{
+}
+
+//-----------------------------------
+// Get the singleton instance
+//-----------------------------------
+GeneticsSimDataParser *GeneticsSimDataParser::getInstance()
+{
+	static GeneticsSimDataParser *theInstance = NULL;
+	if(theInstance == NULL)
+	{
+		theInstance = new GeneticsSimDataParser();
+	}
+	return theInstance;
+}
+
+//-----------------------------------
+// Initialize the data file
+//-----------------------------------
+void GeneticsSimDataParser::initDataFile(char *fileName)
+{
+	char line[128];
 	strcpy(m_sFileName, fileName);
 
 	// Open the data file for reading
@@ -31,7 +60,7 @@ GeneticsSimDataParser::GeneticsSimDataParser(char *fileName)
 		{
 			if(strcmp(line, "<GENE>") == 0)
 			{
-				m_iNumGenes++;	// Count all wells
+				m_iNumGenes++;	// Count all genes
 			}
 			else if(strcmp(line, "<GENUS>") == 0)
 			{
@@ -42,6 +71,11 @@ GeneticsSimDataParser::GeneticsSimDataParser(char *fileName)
 			{
 				getNextLine(line, 127); // Read the species string
 				strcpy(m_sSpecies, line); // Save the species name
+			}
+			else if(strcmp(line, "<CHROMOSOME_COUNT>") == 0)
+			{
+				getNextLine(line, 127); // Read the chromosome count string
+				m_iNumChromosomes = atoi(line); // Save the count
 			}
 			else if(strcmp(line, "<COMMON_NAME>") == 0)
 			{
@@ -61,18 +95,19 @@ GeneticsSimDataParser::GeneticsSimDataParser(char *fileName)
 }
 
 //-----------------------------------
-// Destructor
-//-----------------------------------
-GeneticsSimDataParser::~GeneticsSimDataParser()
-{
-}
-
-//-----------------------------------
 // Get the number of genes
 //-----------------------------------
 int GeneticsSimDataParser::getGeneCount()
 {
 	return m_iNumGenes;
+}
+
+//-----------------------------------
+// Get the number of chromosomes
+//-----------------------------------
+int GeneticsSimDataParser::getChromosomeCount()
+{
+	return m_iNumChromosomes;
 }
 
 //-----------------------------------
@@ -120,12 +155,15 @@ char *GeneticsSimDataParser::getCommonName()
 //						recessive allele name
 //			*recSymbol - pointer to a char variable to hold the recessive
 //						trait letter symbol
+//			*coChance - pointer to double variable to hold the cross-over
+//						probability (range 0.0 to 100.0)
 //--------------------------------------------------------------------------
 bool GeneticsSimDataParser::getGeneData(char *trait, char *domDesc, char *domSymbol, 
-										char *recDesc, char *recSymbol)
+										char *recDesc, char *recSymbol, double *coChance)
 {
 	int gNum = 0;
 	char line[128];
+
 	// Reopen the file
 	inFile = new ifstream();
 	inFile->open(m_sFileName, fstream::in);
@@ -187,6 +225,15 @@ bool GeneticsSimDataParser::getGeneData(char *trait, char *domDesc, char *domSym
 							else
 								return false; // Oops!
 						}
+						else if(strcmp(line, "<CROSSOVER_CHANCE>") == 0)
+						{
+							if(getNextLine(line, 127))
+							{
+								*coChance = atof(line); // Set the cross-over chance
+							}
+							else
+								return false; // Oops!
+						}
 						else if(strcmp(line, "</GENE>") == 0)
 						{
 							m_iNextGeneNumber++; // Increment for next call to this function
@@ -209,66 +256,176 @@ bool GeneticsSimDataParser::getGeneData(char *trait, char *domDesc, char *domSym
 }
 
 //--------------------------------------------------------------------------
-// Get data on the next parent
-// Args:	*genotype - pointer to a character array to hold the genotype
-//						description as a string, e.g. "Tt Ss Ww Cc".  This
-//                      is a pea plant with the phenotype of tall with
-//						wrinkled, green seeds and purple flowers.  Its
-//						genotype if heterozygous tall, heterozygous wrinkled
-//						seeds, heterozygous green seeds, and heterozygous
-//						purple flowers.
+// Get data on the next chromosome for parent1
+// Args:	*strand1 - pointer to a character array to hold the stand1
+//						genotype description as a string, e.g. "T S".  
+//			*strand2 - pointer to a character array to hold the stand2
+//						genotype description as a string, e.g. "t s".
+//  
+//						If this is a pea plant with the phenotype of tall 
+//						with green seeds.  Its phenotype is heterozygous 
+//						tall, and heterozygous green seeds, but these two
+//						genes are linked because they are on the same
+//						chromosome.
 //--------------------------------------------------------------------------
-bool GeneticsSimDataParser::getParentGenotype(char *genotype)
+bool GeneticsSimDataParser::getP1Chromosome(char *strand1, char *strand2)
 {
-	int pNum = 0;
+	int cNum = 0;
 	char line[128];
-	// Limit to only two parents in any one test
-	if(m_iNextParentNumber >= 2) return false;
+
+	// If we have already gotten all the chromosomes for P1 then return false
+	if(m_iNextChNumP1 >= m_iNumChromosomes) return false;
 
 	// Reopen the file
 	inFile = new ifstream();
 	inFile->open(m_sFileName, fstream::in);
 	if(inFile->is_open())
 	{
-		// Read to the the current sensor count
+		// Read to the the first parent
 		while(getNextLine(line, 127))
 		{
-			if(strcmp(line, "<PARENT>") == 0) // Got one
+			if(strcmp(line, "<PARENT>") == 0) // Got it
 			{
-				if(pNum == m_iNextParentNumber)
+				// Get the next chromosome
+				while(getNextLine(line, 127))
 				{
-					// Get data on this one
-					while(getNextLine(line, 127))
+					// Get the sensor type
+					if(strcmp(line, "<CHROMOSOME>") == 0)
 					{
-						// Get the sensor type
-						if(strcmp(line, "<GENOTYPE>") == 0)
+						if(cNum == m_iNextChNumP1) // get this one
 						{
-							if(getNextLine(line, 127)) // Read the genotype string
+							while(getNextLine(line, 127))
 							{
-								strcpy(genotype, line); 
+								if(strcmp(line, "<STRAND1>") == 0)
+								{
+									if(getNextLine(line, 127)) // Read the genotype string
+									{
+										strcpy(strand1, line); 
+									}
+									else
+										return false; // Oops!
+								}
+								else if(strcmp(line, "<STRAND2>") == 0)
+								{
+									if(getNextLine(line, 127)) // Read the genotype string
+									{
+										strcpy(strand2, line); 
+									}
+									else
+										return false; // Oops!
+								}
+								else if(strcmp(line, "</CHROMOSOME>") == 0)
+								{
+									m_iNextChNumP1++;
+									inFile->close();
+									delete inFile; // Delete the istream object not the file
+									return true; // Got one
+								}
 							}
-							else
-								return false; // Oops!
 						}
-						else if(strcmp(line, "</PARENT>") == 0)
+						else
 						{
-							m_iNextParentNumber++; // Increment for next call to this function
-							inFile->close();
-							delete inFile; // Delete the istream object not the file
-							return true; // Got one
+							cNum++;
 						}
-					} // end while
-				} // end if(pNum == nextParentNum)
-				else
-				{
-					pNum++; // Check the next one
-				}
-			}
-		}
+					}
+				} // End while(getNextLine 
+			} // end if PARENT tag
+		} // End while(getNextLine
 		inFile->close();
 		delete inFile; // Delete the istream object not the file
 	} // end if file open
-	return false; // If we get here we have got all the parents or failed to open file
+	return false; // If we get here we have got all the chromosomes or failed to open file
+}
+
+//--------------------------------------------------------------------------
+// Get data on the next chromosome for parent2
+// Args:	*strand1 - pointer to a character array to hold the stand1
+//						genotype description as a string, e.g. "T S".  
+//			*strand2 - pointer to a character array to hold the stand2
+//						genotype description as a string, e.g. "t s".
+//  
+//						If this is a pea plant with the phenotype of tall 
+//						with green seeds.  Its phenotype is heterozygous 
+//						tall, and heterozygous green seeds, but these two
+//						genes are linked because they are on the same
+//						chromosome.
+//--------------------------------------------------------------------------
+bool GeneticsSimDataParser::getP2Chromosome(char *strand1, char *strand2)
+{
+	int cNum = 0;
+	char line[128];
+	bool foundFirst = false;
+
+	// If we have already gotten all the chromosomes for P2 then return false
+	if(m_iNextChNumP2 >= m_iNumChromosomes) return false;
+
+	// Reopen the file
+	inFile = new ifstream();
+	inFile->open(m_sFileName, fstream::in);
+	if(inFile->is_open())
+	{
+		// Read to the the first parent
+		while(getNextLine(line, 127) && !foundFirst)
+		{
+			if(strcmp(line, "<PARENT>") == 0) // Got it
+			{
+				foundFirst = true;
+			}
+		}
+		// Now Read to the the second parent
+		while(getNextLine(line, 127))
+		{
+			if(strcmp(line, "<PARENT>") == 0) // Got it
+			{
+				// Get the next chromosome
+				while(getNextLine(line, 127))
+				{
+					// Get the sensor type
+					if(strcmp(line, "<CHROMOSOME>") == 0)
+					{
+						if(cNum == m_iNextChNumP2) // get this one
+						{
+							while(getNextLine(line, 127))
+							{
+								if(strcmp(line, "<STRAND1>") == 0)
+								{
+									if(getNextLine(line, 127)) // Read the genotype string
+									{
+										strcpy(strand1, line); 
+									}
+									else
+										return false; // Oops!
+								}
+								else if(strcmp(line, "<STRAND2>") == 0)
+								{
+									if(getNextLine(line, 127)) // Read the genotype string
+									{
+										strcpy(strand2, line); 
+									}
+									else
+										return false; // Oops!
+								}
+								else if(strcmp(line, "</CHROMOSOME>") == 0)
+								{
+									m_iNextChNumP2++;
+									inFile->close();
+									delete inFile; // Delete the istream object not the file
+									return true; // Got one
+								}
+							}
+						}
+						else
+						{
+							cNum++;
+						}
+					}
+				} // End while(getNextLine 
+			} // end if PARENT tag
+		} // End while(getNextLine
+		inFile->close();
+		delete inFile; // Delete the istream object not the file
+	} // end if file open
+	return false; // If we get here we have got all the chromosomes or failed to open file
 }
 
 //------------------------------------------------
--- instructor_files/ReadMe.txt
+++ instructor_files/ReadMe.txt
@@ -1,12 +1,18 @@
 This zip file contains the following files:
 
-ReadMe.txt - The file you are currently reading.
+ReadMe.txt - The file you are reading now.
 
-GeneticsSim1.xml - Data file to be used with program 1. This is not a fully
+GeneticsSim2a.xml - Data file to be used with program 2. This is not a fully
 	qualified XML file so it cannot be opened with a standard XML reader.
+	The organism in this data file is Pisum Sativum, the pea plant.
+
+GeneticsSim2b.xml - Data file to be used with program 2. This is not a fully
+	qualified XML file so it cannot be opened with a standard XML reader.
+	The organism in this data file is Drosophila Melanogaster, the fruit fly.
 
 GeneticsSimDataParser.h and GeneticsSimDataParser.cpp - A data parser class you can
-	use to read and get data from the GeneticsSim1.xml file.
+	use to read and get data from the GeneticsSim2.xml file.  This class is implemented
+	following the Singleton Design Pattern.
 
-Prog1.txt - This is actually a demonstration executable file of program 1.  To run
+Prog2.txt - This is actually a demonstration executable file of program 2.  To run
 	the program change the ".txt" extension to ".exe" and double click the .exe file.
