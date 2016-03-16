2250c2646e528e2f039cd4ddeec2b4e92a516166
Marcus Posey <marcusposey@gmail.com>
2016-03-15 23:43:43 -0500

    Created full MasterGene class and tests

--- Genetics/building_blocks/MasterGene.cpp
+++ Genetics/building_blocks/MasterGene.cpp
@@ -1,1 +1,2 @@
 #include "MasterGene.h"
+
--- tests/master_gene_tests.cpp
+++ tests/master_gene_tests.cpp
@@ -1,5 +1,17 @@
 #include <gtest/gtest.h>
 
-#include "../Genetics/building_blocks/MasterGene.h"
+#include "../Genetics/building_blocks/MasterGene.cpp"
 
 class MasterGeneTests : public testing::Test {};
+
+/**
+ * This test expects that supplying negative crossover chance results in
+ * the field being set to zero.
+ *
+ * Deviation from the above should result in a failed test.
+ */
+TEST_F(MasterGeneTests, negativeCrossoverChance) {
+	MasterGene gene("", "", "", ' ', ' ', -27.2);
+
+	ASSERT_EQ(0, gene.getCrossoverChance());
+}
