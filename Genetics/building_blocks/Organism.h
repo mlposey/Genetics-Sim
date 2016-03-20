258ecbd83970f4227f71a6a5e91c5771b1449db0
Marcus Posey <marcusposey@gmail.com>
2016-03-20 01:14:27 -0500

    Fixed started shift to v2.0 documentation compliance

--- Genetics/building_blocks/Organism.h
+++ Genetics/building_blocks/Organism.h
@@ -9,7 +9,6 @@
 #pragma once
 #include <vector>
 
-#include "gene/Gene.h"
 #include "Chromosome.h"
 
 using std::string;
@@ -18,40 +17,40 @@ using std::string;
  * @brief The Organism class represents a living thing
  *
  * The main purpose of the Organism is to store categorical information,
- * such as scientific name, as well as the genotype. New organisms can be
- * created through use of the LoveChamber class and two parent organisms.
- * @see Gene, LoveChamber
+ * such as scientific name, as well as the genotype. New organisms should be
+ * created with OrganismFactory.
+ * @see Gene
  */
 class Organism
 {
 public:
 	/**
-	 * @brief Constructs an Organism with an initial genus, species, name, and blank genotype
+	 * @brief Constructs an Organism with an initial genus, species, and name
 	 *
-	 * As the initial state of the Organism is one without a genotype, it is
-	 * necessary that ::addGene is used to assign the Organism some genotype
-	 * which then makes it elligble to mate with other organisms.
+	 * The initial state is an Organism without Chromosomes. They can be added
+	 * using ::addChromosome.
 	 * @param genus the genus of the organism
 	 * @param species the species of the organism
 	 * @param name the common name of the organism
 	 */
 	Organism(const string &genus, const string &species, const string &name);
 
-	/**
-	 * Adds a gene to the Organism's genotype
-	 * @param gene a constant reference to a Gene to be added to the genotype
-	 */
-	void addGene(const Gene &gene);
 
 	/**
-	 * @brief Retrieves the next Gene in the genotype
-	 *
-	 * Repeated calls to ::serveGene will iterate through the Organism's list of
-	 * Genes. When the list has been fully traversed, the position returns to
-	 * zero.
-	 * @return the next gene in the organism's genotype
+	 * @brief Adds a Chromosome to the Organism's genetic makeup
+	 * @param c The chromosome to add
 	 */
-	Gene &serveGene();
+	void addChromosome(const Chromosome &c);
+
+	/**
+	 * @brief Retrieves the next Chromosome
+	 *
+	 * Repeated calls to ::serveChromosome will iterate through the Organism's list of
+	 * Chromosomes. When the list has been fully traversed, the position returns to
+	 * zero.
+	 * @return the next chromosome in the organism's genotype
+	 */
+	Chromosome &serveChromosome();
 
 	/**
 	 * @brief Prints a full description of the Organism to stdout
@@ -61,11 +60,11 @@ public:
 	 */
 	void printDescription() const;
 
-	/// Returns the number of Genes in the Organism's genotype
-	int getGeneCount() const { return _genotype.size(); }
+	/// Returns the number of Chromosomes in the Organism
+	int getChromosomeCount() const { return _chromosomes.size(); }
 
-	/// Returns a const reference to the Organism's genotype
-	const std::vector<Gene> &getGenotype() const { return _genotype; };
+	/// Returns the Organism's collection of Chromosome objects
+	const std::vector<Chromosome> &getChromosomes() const { return _chromosomes; }
 
 	/// Returns the genus of the the Organism as a string
 	string getGenus() const { return _genus; }
@@ -77,11 +76,13 @@ public:
 	string getName() const { return _name; }
 
 private:
+	// The position of the next Chromosome to serve from _chromosomes
 	int _serveCounter;
 
 	string _genus;
 	string _species;
 	string _name;
 
+	// The collection of chromosomes that make up the Organism
 	std::vector<Chromosome> _chromosomes;
 };
