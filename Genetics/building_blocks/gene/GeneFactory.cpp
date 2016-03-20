3206a4e2cf2580c33f1b312212884b3a54644c70
Marcus Posey <marcusposey@gmail.com>
2016-03-20 14:04:47 -0500

    Removed threading to aid in debugging allele symbol issue

--- Genetics/building_blocks/gene/GeneFactory.cpp
+++ Genetics/building_blocks/gene/GeneFactory.cpp
@@ -3,10 +3,14 @@
 #include "../../utils/CustomExceptions.h"
 
 Gene GeneFactory::createGene(const string& allele1, const string& allele2) {
-	auto masterGene = MasterGeneIndex::getInstance()->get(allele1);
+	MasterGeneIndex *index = MasterGeneIndex::getInstance();
+	auto masterGene = index->get(allele1);
+	// Temporarily removed to aid in debugging
+	// auto masterGene = MasterGeneIndex::getInstance()->get(allele1);
 	if (masterGene == nullptr) {
 		// It doesn't matter which allele would not exist, since they both should
-		throw InvalidSymbolException(allele1 + " does not belong to a MasterGene");
+		throw InvalidSymbolException("Allele '" + allele1
+									 + "' does not belong to a MasterGene");
 	}
 	return Gene(masterGene, allele1, allele2);
 }
--- Genetics/core/Simulation.cpp
+++ Genetics/core/Simulation.cpp
@@ -115,6 +115,7 @@ void Simulation::loadParentData() {
 		s.strand2 = buffer[1];
 	}
 
+	/*
 	// Build the first parent using factories
 	std::thread parent1Thread([&]() {
 		_parent1 = OrganismFactory::getInstance()->createOrganism(
@@ -134,5 +135,15 @@ void Simulation::loadParentData() {
 	});
 
 	parent1Thread.join();
-	parent2Thread.join();
+	parent2Thread.join();*/
+	_parent1 = OrganismFactory::getInstance()->createOrganism(
+			parser->getGenus(),
+			parser->getSpecies(),
+			parser->getCommonName(),
+			p1);
+	_parent2 = OrganismFactory::getInstance()->createOrganism(
+			parser->getGenus(),
+			parser->getSpecies(),
+			parser->getCommonName(),
+			p2);
 }
