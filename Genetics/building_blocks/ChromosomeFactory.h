05e72a41cfec029e2c5a077db80c2363887c6dff
Marcus Posey <marcusposey@gmail.com>
2016-03-20 04:55:25 -0500

    Documented method and class

--- Genetics/building_blocks/ChromosomeFactory.h
+++ Genetics/building_blocks/ChromosomeFactory.h
@@ -3,10 +3,19 @@
 #include "../utils/macros.h"
 #include "Chromosome.h"
 
+/**
+ * @brief The ChromosomeFactory class creates Chromosome objects
+ * @see Chromosome
+ */
 class ChromosomeFactory
 {
 	SINGLETON(ChromosomeFactory)
 
 public:
+	/**
+	 * @brief Creates and returns a Chromosome object
+	 * @param rc A RawChromosome containing basic strand information
+	 * @return A chromosome object containing information from rc
+	 */
 	Chromosome createChromosome(const RawChromosome &rc) const;
 };
