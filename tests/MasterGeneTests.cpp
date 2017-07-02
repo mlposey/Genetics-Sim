5a51561a28272c527972ffb1139eff5d881406ee
Marcus Posey <marcusposey@gmail.com>
2017-07-01 20:19:46 -0500

    Write test for MasterGene constructor

---
+++ tests/MasterGeneTests.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "../src/building_blocks/gene/MasterGene.cpp"
+
+// MasterGene.MasterGene should round negative crossover chances to zero
+// and keep values which were already positive.
+TEST(MasterGeneTest, forcePositiveCrossoverChance) {
+    MasterGene negativeGene("","","",' ', ' ', -3.2);
+    ASSERT_EQ(0, negativeGene.getCrossoverChance());
+
+    MasterGene positiveGene("","","",' ', ' ', 2);
+    ASSERT_EQ(2, positiveGene.getCrossoverChance());
+
+    MasterGene zeroGene("","","",' ', ' ', -3.2);
+    ASSERT_EQ(0, zeroGene.getCrossoverChance());
+}
+
