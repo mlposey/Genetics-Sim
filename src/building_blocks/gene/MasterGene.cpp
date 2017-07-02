069771682a52fb401b096f0604ba30f44e8106df
Marcus Posey <marcusposey@gmail.com>
2017-07-01 20:07:28 -0500

    Move constructor definition to source file

--- src/building_blocks/gene/MasterGene.cpp
+++ src/building_blocks/gene/MasterGene.cpp
@@ -1,2 +1,15 @@
 #include "MasterGene.h"
 
+MasterGene::MasterGene(const string &trait, const string &dominantAllele,
+           const string &recessiveAllele, char dominantSymbol,
+           char recessiveSymbol, double crossoverChance)
+    : _trait(trait)
+    , _dominantAllele(dominantAllele)
+    , _recessiveAllele(recessiveAllele)
+    , _dominantSymbol(dominantSymbol)
+    , _recessiveSymbol(recessiveSymbol)
+    , _crossoverChance(crossoverChance) {
+        // This must be at least 0 since the concept of a negative chance
+        // does not make sense.
+        _crossoverChance = _crossoverChance < 0 ? 0 : _crossoverChance;
+}
--- src/building_blocks/gene/MasterGene.h
+++ src/building_blocks/gene/MasterGene.h
@@ -21,17 +21,7 @@ class MasterGene
 public:
 	MasterGene(const string &trait, const string &dominantAllele,
 		       const string &recessiveAllele, char dominantSymbol,
-			   char recessiveSymbol, double crossoverChance)
-		: _trait(trait)
-		, _dominantAllele(dominantAllele)
-		, _recessiveAllele(recessiveAllele)
-		, _dominantSymbol(dominantSymbol)
-		, _recessiveSymbol(recessiveSymbol)
-		, _crossoverChance(crossoverChance) {
-		// We set negative crossover chances to 0 since the concept
-		// of a negative chance does not make sense.
-		_crossoverChance = _crossoverChance < 0 ? 0 : _crossoverChance;
-	}
+			   char recessiveSymbol, double crossoverChance);
 
 	string getTrait() const {
 		return _trait;
