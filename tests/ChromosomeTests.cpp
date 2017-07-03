998593e7b1861f84c6d81c9a922ea7627084e9ec
Marcus Posey <marcusposey@gmail.com>
2017-07-03 08:09:04 -0500

    Write tests for Chromosome

---
+++ tests/ChromosomeTests.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "core/Chromosome.cpp"
+#include "core/gene/MasterGeneIndex.h"
+
+// Chromosome.addGene should place the first allele of the gene on the strand
+// labeled Strand.STRAND1 and the second allele of the gene on the strand
+// labeled Strand.STRAND2.
+TEST(ChromosomeTests, ensureStrandPlacement) {
+    auto index = MasterGeneIndex::getInstance();
+    index->clear();
+
+    char sym1 = 'T', sym2 = 't';
+    auto masterGene = std::make_shared<MasterGene>(
+            "", "", "", sym1, sym2, 3.3
+    );
+    index->add(masterGene);
+
+    Chromosome chromosome;
+    chromosome.addGene({masterGene, sym1, sym2});
+
+    ASSERT_TRUE(Chromosome::Strand::STRAND1 == chromosome.getStrand(sym1));
+    ASSERT_TRUE(Chromosome::Strand::STRAND2 == chromosome.getStrand(sym2));
+}
+
