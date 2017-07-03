ae49fb15d2f14b901de9ad1a6be4aa1c7df464ac
Marcus Posey <marcusposey@gmail.com>
2017-07-03 08:36:01 -0500

    Ensure Organism.serveChromosome loops

--- tests/OrganismTests.cpp
+++ tests/OrganismTests.cpp
@@ -2,17 +2,45 @@
 #include "core/Organism.cpp"
 #include "util/CustomExceptions.h"
 
-class OrganismTest : public testing::Test {};
+class OrganismTest : public testing::Test {
+public:
+    OrganismTest() : organism("test", "test", "test") {}
+    Organism organism;
+};
 
 // Organism.serveChromosome should throw EmptyContainerException if the
 // organism has no chromosomes.
 TEST_F(OrganismTest, serveChromosome_empty) {
-    Organism organism("test", "test", "test");
     ASSERT_THROW(organism.serveChromosome(), EmptyContainerException);
 }
 
 // Organism.serveChromosome should loop to the start of the chromosomes
 // after the collection has been fully traversed.
 TEST_F(OrganismTest, serveChromosome_loop) {
-    // TODO:
+    // Create chromosomes to add to the organism.
+    auto index = MasterGeneIndex::getInstance();
+    index->clear();
+
+    char sym1 = 'T', sym2 = 't';
+    auto masterGene = std::make_shared<MasterGene>(
+            "", "", "", sym1, sym2, 3.3
+    );
+    index->add(masterGene);
+
+    Chromosome chromosome1, chromosome2;
+    chromosome1.addGene({masterGene, sym1, sym2});
+    chromosome2.addGene({masterGene, sym1, sym1});
+
+    organism.addChromosome(chromosome1);
+    organism.addChromosome(chromosome2);
+
+    // TODO: This test really isn't ideal because we can't be sure the last
+    //  call to serveChromosome returns the same chromosome as the first call.
+    //  Fix this by overloading the equality operator for Chromosome and using
+    //  ASSERT_EQ to verify the chromosome's identity.
+
+    // Expect to loop through from chromosome1 -> chromosome2 -> chromosome1
+    organism.serveChromosome();
+    organism.serveChromosome();
+    ASSERT_NO_THROW(organism.serveChromosome());
 }
