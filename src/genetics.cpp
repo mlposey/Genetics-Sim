d89ad10f42b66b0b3e154b83f61703900c51c3a1
Marcus Posey <marcusposey@gmail.com>
2017-07-03 00:28:05 -0500

    Remove assignment header

--- src/genetics.cpp
+++ src/genetics.cpp
@@ -1,15 +1,6 @@
-/*******************************************************************
-*   genetics.cpp
-*   CS 307 Programming Assignment 1
-*   Author: Marcus Posey
-*   Date Submitted: 02/23/2016
-*
-*   This program is entirely my own work.
-*******************************************************************/
 #include "core/Simulation.h"
 
 int main() {
 	Simulation().run();
-
 	return 0;
 }
